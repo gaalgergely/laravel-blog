3112d9cb4b2e841d8748eb8b1f228cd74f9e5b65
Gaál Gergely <contact@gaalgergely.net>
2019-05-11 17:42:07 +0200

    Display all posts to the blog index

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function getImageUrlAttribute($value){
+
+        $imageUrl = '';
+
+        if(!is_null($this->image))
+        {
+            if(file_exists(public_path() . '/img/' . $this->image)){
+                $imageUrl = asset('img/' . $this->image);
+            }
+        }
+
+        return $imageUrl;
+    }
 }
