d10b2ca78ad363ea46691edba60add5bb3575a75
Gaál Gergely <contact@gaalgergely.net>
2019-05-12 12:59:12 +0200

    Filtering posts by category

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 use App\Post;
 
@@ -10,8 +11,25 @@ class BlogController extends Controller
     protected $limit = 5;
 
     public function index(){
+
+        $categories = Category::with(['posts' => function($query){
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
         $posts = Post::with('author')->latestFirst()->published()->simplePaginate($this->limit);
-        return view('blog.index', compact('posts'));
+
+        return view('blog.index', compact('posts', 'categories'));
+    }
+
+    public function category($id){
+
+        $categories = Category::with(['posts' => function($query){
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
+        $posts = Post::with('author')->latestFirst()->published()->where('category_id', $id)->simplePaginate($this->limit);
+
+        return view('blog.index', compact('posts', 'categories'));
     }
 
     public function show(Post $post)
--- app/Post.php
+++ app/Post.php
@@ -15,6 +15,10 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
     public function getImageUrlAttribute(){
 
         $imageUrl = '';
