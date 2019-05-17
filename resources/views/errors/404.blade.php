94e47c648e19443e34ddf574f4cfe166784221a7
Gaál Gergely <contact@gaalgergely.net>
2019-05-17 17:53:24 +0200

    Custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <p>{{ __('Not Found') }}</p>
+        </div>
+    </div>
+</div>
+@endsection
