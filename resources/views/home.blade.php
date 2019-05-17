8d403a68b055d56913743c2089fd808352d58c7e
Gaál Gergely <contact@gaalgergely.net>
2019-05-17 21:00:15 +0200

    Admin layout is set & logout actions works

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.backend.main')
+
+@section('title', 'MyBlog | Dashboard')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Dasbhboard
+            </h1>
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+            </ol>
+        </section>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="box">
+                        <!-- /.box-header -->
+                        <div class="box-body ">
+                            <h3>Welcome to MyBlog!</h3>
+                            <p class="lead text-muted">Hallo {{ Auth::user()->name }}, Welcome to MyBlog</p>
+
+                            <h4>Get started</h4>
+                            <p><a href="#" class="btn btn-primary">Write your first blog post</a> </p>
                         </div>
-                    @endif
-
-                    You are logged in!
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
                 </div>
             </div>
-        </div>
+            <!-- ./row -->
+        </section>
+        <!-- /.content -->
     </div>
-</div>
 @endsection
---
+++ resources/views/layouts/backend/navbar.blade.php
@@ -0,0 +1,51 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="/" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>M</b>B</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>MY</b>BLOG</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <!-- <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a> -->
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="/backend/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                        <span class="hidden-xs">{{ Auth::user()->name }}</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <img src="/backend/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                            <p>
+                                {{ Auth::user()->name }} - Web Developer
+                                <small>Member since Nov. 2012</small>
+                            </p>
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="{{ route('logout') }}" class="btn btn-default btn-flat" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign out</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
